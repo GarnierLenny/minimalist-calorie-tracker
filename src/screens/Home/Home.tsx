862a695223e50dd5723cec56a5296c0b56b33faa
Lenny Garnier <lenny.garnier00@gmail.com>
2024-01-23 02:49:30 +0900

    Improved date change ui?ux

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -6,7 +6,7 @@ import styles from "./Home.styles";
 import { RootStackParamList } from "../../../App";
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import Animated, { useSharedValue, FlipInEasyX, FlipOutEasyX } from 'react-native-reanimated';
+import Animated, { useSharedValue, FlipInEasyX, FlipOutEasyX, withTiming, Easing, useAnimatedStyle, withSpring } from 'react-native-reanimated';
 import { unit, getSelectedType, editAmountButtonProps } from "./Home.types";
 import { formatDate } from "../../utils/formatDate";
 
@@ -86,11 +86,23 @@ const HomeScreen = ({ navigation }: NativeStackScreenProps<RootStackParamList, '
     getCalories('water', setWater);
   }, [date]);
 
+  const rotateX = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => (
+    {transform: [{ perspective: 500 }, { rotateX: `${rotateX.value}deg` }]}
+  ));
+
+
   const changeDateHandler = (increment: number) => {
     setDate(oldDate => {
       const newDate = new Date(oldDate);
 
-      newDate.setDate(newDate.getDate() + increment);
+      rotateX.value = withTiming(90, { duration: 100, easing: Easing.linear }, () => {
+        rotateX.value = withTiming(0, { duration: 100, easing: Easing.linear });
+      });
+      setTimeout(() => {
+        newDate.setDate(newDate.getDate() + increment);
+      }, 70);
       return newDate;
     })
   }
@@ -151,10 +163,7 @@ const HomeScreen = ({ navigation }: NativeStackScreenProps<RootStackParamList, '
     });
   };
 
-  const [animate, setAnimate] = useState<boolean>(false);
-
   const changeSelected = async (newSelected: string) => {
-    setAnimate(true);
     setSelected(newSelected);
     AsyncStorage.setItem('last-selected', newSelected);
   };
@@ -176,8 +185,11 @@ const HomeScreen = ({ navigation }: NativeStackScreenProps<RootStackParamList, '
       {/* date section*/}
       <SafeAreaView style={styles.dateContainer}>
         <IconButton name="arrow-left-bold-circle" size={25} callback={() => changeDateHandler(-1)} />
-        <Text style={styles.dateText}>{date.getDate()}/{date.getMonth() + 1}/
-        {date.getFullYear()}</Text>
+        <SafeAreaView style={{backgroundColor: '#fff', borderRadius: 10}}>
+          <Animated.View style={[{paddingHorizontal: 10, paddingVertical: 10}, animatedStyle]}>
+            <Text style={styles.dateText}>{date.getDate()}/{date.getMonth() + 1}/{date.getFullYear()}</Text>
+          </Animated.View>
+        </SafeAreaView>
         <IconButton color='#000' disabled={formatDate(date) === referenceDate ? true : false} name="arrow-right-bold-circle" size={25} callback={() => changeDateHandler(+1)} />
       </SafeAreaView>
       {/* 0g / 2100g section*/}
@@ -202,19 +214,19 @@ const HomeScreen = ({ navigation }: NativeStackScreenProps<RootStackParamList, '
       {/* Change selected intake section*/}
       <SafeAreaView style={{flexDirection: 'row', marginTop: 50, justifyContent: 'space-evenly', width: '70%', alignSelf: 'center'}}>
         {selected === 'calories' ?
-          <Animated.View style={{backgroundColor: '#000', paddingVertical: 5, paddingHorizontal: 10, borderRadius: 15}} entering={animate ? FlipInEasyX : undefined} exiting={animate ? FlipOutEasyX : undefined}>
+          <Animated.View style={{backgroundColor: '#000', paddingVertical: 5, paddingHorizontal: 10, borderRadius: 15}} entering={FlipInEasyX} exiting={FlipOutEasyX}>
             <Text style={{fontWeight: '600', color: '#fff'}}>Calories</Text>
           </Animated.View>
           :
           <ChangeSelectButton targetSelection="calories" />}
         {selected === 'proteins' ?
-          <Animated.View style={{backgroundColor: '#000', paddingVertical: 5, paddingHorizontal: 10, borderRadius: 15}} entering={animate ? FlipInEasyX : undefined} exiting={animate ? FlipOutEasyX : undefined}>
+          <Animated.View style={{backgroundColor: '#000', paddingVertical: 5, paddingHorizontal: 10, borderRadius: 15}} entering={FlipInEasyX} exiting={FlipOutEasyX}>
             <Text style={{fontWeight: '600', color: '#fff'}}>Proteins</Text>
           </Animated.View>
           :
           <ChangeSelectButton targetSelection="proteins" />}
         {selected === 'water' ?
-          <Animated.View style={{backgroundColor: '#000', paddingVertical: 5, paddingHorizontal: 10, borderRadius: 15}} entering={animate ? FlipInEasyX : undefined} exiting={animate ? FlipOutEasyX : undefined}>
+          <Animated.View style={{backgroundColor: '#000', paddingVertical: 5, paddingHorizontal: 10, borderRadius: 15}} entering={FlipInEasyX} exiting={FlipOutEasyX}>
             <Text style={{fontWeight: '600', color: '#fff'}}>Water</Text>
           </Animated.View>
           :
