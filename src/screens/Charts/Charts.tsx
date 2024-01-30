76dfd24772872db4daf82b3203910ae4407247f7
GarnierLenny <61317098+GarnierLenny@users.noreply.github.com>
2024-01-31 04:09:56 +0900

    added circles section

--- src/screens/Charts/Charts.tsx
+++ src/screens/Charts/Charts.tsx
@@ -1,14 +1,46 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, Dimensions } from 'react-native';
 import ChangeSection from '../Home/components/ChangeSection.component';
 import Heatmap from './components/Heatmap.component';
+import { ProgressChart } from 'react-native-chart-kit';
+
+const Circles = () => {
+  const data = {
+    labels: ['Calories', 'Proteins', 'Water'],
+    data: [0.4, 0.6, 0.8]
+  };
+  const screenWidth = Dimensions.get('screen').width;
+
+  const chartConfig = {
+    color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientToOpacity: 0,
+    strokeWidth: 3,
+  };
+
+  return (
+    <SafeAreaView>
+      <ProgressChart
+        data={data}
+        width={screenWidth}
+        height={220}
+        strokeWidth={16}
+        radius={32}
+        chartConfig={chartConfig}
+        hideLegend={false}
+      />
+    </SafeAreaView>
+  );
+};
 
 const ChartsScreen = () => {
-return (
+  return (
     <SafeAreaView style={styles.mainContainer}>
+      <Text style={styles.dailyText}>Past 7 days average</Text>
+      <Circles />
       <Text style={styles.dailyText}>Daily consistency</Text>
       <Heatmap />
-      <SafeAreaView style={{marginTop: 0}}>
+      <SafeAreaView style={{marginTop: 10}}>
         <ChangeSection />
       </SafeAreaView>
     </SafeAreaView>
@@ -16,7 +48,15 @@ return (
 };
 
 const styles = StyleSheet.create({
-  mainContainer: {
+  dailyText: {
+    fontSize: 16,
+    marginTop: 25,
+    marginBottom: 20,
+    fontWeight: '600',
+    marginLeft: 10,
+  },
+  mainContainer:
+  {
     display: 'flex',
   },
   timespanContainer: {
@@ -29,12 +69,6 @@ const styles = StyleSheet.create({
   },
   timespanText: {
     fontSize: 20,
-  },
-  dailyText: {
-    fontSize: 16,
-    fontWeight: '600',
-    marginVertical: 10,
-    marginLeft: 10,
   },
 });
 
